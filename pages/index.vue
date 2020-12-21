0c9a90075d1d2f1c528b21f2f441dd3b4f522806
nagi125 <nagi125.dev@gmail.com>
2020-12-21 23:22:27 +0900

    indexにUser一覧を確認できる機能を追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="animated fadeIn">
-    認証成功ページ
+    <b-row>
+      <b-col md="12">
+        <b-button variant="primary" @click="displayUsers">User表示</b-button>
+      </b-col>
+    </b-row>
+    <p class="my-3 h4 font-weight-bold text-primary">{{ count }}件</p>
+    <div v-if="isLoading" class="text-center">
+      <b-spinner variant="primary" label="Spinning"></b-spinner>
+    </div>
+    <b-row v-if="items.length !== 0">
+      <b-col lg="12">
+        <b-card v-for="(item, index) in items" class="mb-4">
+          <div slot="header" class="font-weight-bold">
+            User{{ index + 1 }}
+          </div>
+          <b-card-body class="py-0 px-2">
+            <p class="my-3"><span class="font-weight-bold">ID: </span>{{ item.id }}</p>
+            <p class="my-3"><span class="font-weight-bold">物件名: </span>{{ item.name }}</p>
+          </b-card-body>
+        </b-card>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -9,8 +30,11 @@ export default {
   data() {
     return {
       header: {
-        title: 'トップページ',
+        title: 'サンプルページ',
       },
+      count: 0,
+      items: [],
+      isLoading: false,
     }
   },
 
@@ -21,6 +45,21 @@ export default {
   methods: {
     setHeader() {
       this.$nuxt.$emit('setHeader', this.header);
+    },
+
+    async displayUsers() {
+      this.isLoading = true;
+
+      try {
+        const res = await this.$axios.get('/api/admin/users');
+
+        this.count = res.data.length;
+        this.items = res.data;
+
+      } catch (e) {
+        console.log(e);
+      }
+      this.isLoading = false;
     },
   }
 }
